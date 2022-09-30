c925624d09d1c43a7a4b6a82bbf5ee2a40c10419
Dmy <dmy@MacBook-Pro-Dmy.local>
2022-09-30 12:34:15 +0300

    new surround for server

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import DIFCClient from './client';
+
+function App () {
+  window.DIFCClient = DIFCClient;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <body>
+          <h1>Hello, please look at the console!</h1>
+          <h3>Get ready for new research!</h3>
+        </body>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/example/example.js
+++ src/example/example.js
@@ -7,7 +7,10 @@ const express = require('express')
 let cors = require('cors')
 const path = require('path')
 const bodyParser = require('body-parser');
-let PORT = 3080
+let ejs = require('ejs');
+const Handlebars = require("handlebars");
+const template = Handlebars.compile("Name: {{name}}");
+let PORT = 3080;
 
 const DIFCClient = require('../client')
 const app = express()
@@ -20,54 +23,77 @@ app.use(cors())
 
 app.get('/', function (req, res) {
   // console.log('LIVEEE')
-  res.sendFile(path.join(__dirname, 'index.html'));
+  // res.sendFile(path.join(__dirname, 'index.html'));
+  let DIFCClientt = require('../client')
   // res.sendFile(path.join(__dirname, 'views', 'index.html'));
-  // res.send('Get ready for new research! Delivered by Deca4');
-  // res.send('Delivered to deca4 By Dmytriy and Zahar!');
+  res.send(`
+  <!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <title>Library Example</title>
+</head>
+
+<body>
+<script type="module" src="example.js"></script>
+<script> window.aaa = ${6}</script>
+  <h1>Hello, please look at the console!</h1>
+  <h3>Get ready for new research!</h3>
+</body>
+
+</html>
+  
+  
+`);
+  // res.render('/', {text1: DIFCClientt});
+  // res.send(DIFCClient);
 })
 app.listen(app.get('port'), function () {
+
   console.log('Node app is running on http://localhost:' + PORT + '');
 })
 
 
 //////////////////////////////
-async function test() {
-  const newContractId = '0.0.47856905'
-  const addr = '0000000000000000000000000000000002d94f1a'
+// async function test() {
+//   const newContractId = '0.0.47856905'
+//   const addr = '0000000000000000000000000000000002d94f1a'
 
-  // set inheriteeKey
-  await SetInheriteeKey(newContractId)
-  // set inheritee as inheritor
-  await SetInheritor(newContractId, addr, addr)
+//   // set inheriteeKey
+//   await SetInheriteeKey(newContractId)
+//   // set inheritee as inheritor
+//   await SetInheritor(newContractId, addr, addr)
 
-  const inheritorKey = await GetInheritorKey(newContractId, addr)
-  console.log('The inheritor key is ' + inheritorKey)
+//   const inheritorKey = await GetInheritorKey(newContractId, addr)
+//   console.log('The inheritor key is ' + inheritorKey)
 
-  let fileCount = await GetFileCount(newContractId, addr)
-  console.log(`Initial file count ${fileCount}`)
+//   let fileCount = await GetFileCount(newContractId, addr)
+//   console.log(`Initial file count ${fileCount}`)
 
-  await AddFile(
-    newContractId,
-    JSON.stringify({
-      name: 'test',
-      location: 'test loc',
-      hash: 'test hash'
-    })
-  )
-  fileCount = await GetFileCount(newContractId, addr)
-  console.log(`File count ${fileCount}`)
+//   await AddFile(
+//     newContractId,
+//     JSON.stringify({
+//       name: 'test',
+//       location: 'test loc',
+//       hash: 'test hash'
+//     })
+//   )
+//   fileCount = await GetFileCount(newContractId, addr)
+//   console.log(`File count ${fileCount}`)
 
-  const file = await GetFile(newContractId, addr, 0)
-  console.log(`File contents ${file}`)
+//   const file = await GetFile(newContractId, addr, 0)
+//   console.log(`File contents ${file}`)
 
-  const content =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  const encryptedFile = await EncryptFile(content, inheritorKey)
-  const hashEncryptedFile = await CalculateHash(encryptedFile)
-  console.log(`Hash encrypted file: ${hashEncryptedFile}`)
+//   const content =
+//     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+//   const encryptedFile = await EncryptFile(content, inheritorKey)
+//   const hashEncryptedFile = await CalculateHash(encryptedFile)
+//   console.log(`Hash encrypted file: ${hashEncryptedFile}`)
 
-  const decryptedFile = await DecryptFile(encryptedFile, inheritorKey)
-  const hashDecryptedFile = await CalculateHash(decryptedFile)
-  console.log(`Hash decriptedFile file: ${hashDecryptedFile}`)
-}
-test()
+//   const decryptedFile = await DecryptFile(encryptedFile, inheritorKey)
+//   const hashDecryptedFile = await CalculateHash(decryptedFile)
+//   console.log(`Hash decriptedFile file: ${hashDecryptedFile}`)
+// }
+// test()
+module.exports = DIFCClient;
